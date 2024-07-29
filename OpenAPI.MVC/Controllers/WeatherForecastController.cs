1607ea189ec311bfdeb29310d23d3b71c7a9eea2
markjensen <donnib@gmail.com>
2024-07-29 21:05:14 +0200

    init commit

---
+++ OpenAPI.MVC/Controllers/WeatherForecastController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace OpenAPI.MVC.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+[SwaggerTag("Create and read WeatherForecast")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Cities = new[]
+{
+        "San Fransico", "New York", "London", "Paris", "Berlin", "Tokyo"
+    };
+
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Test 2
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet(Name = "GetWeatherForecast")]
+    [SwaggerResponse(StatusCodes.Status201Created, "Returns the newly created item")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "If the item is null")]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "Oops!")]
+    public IEnumerable<WeatherForecast> Get(
+         [FromQuery, SwaggerParameter("Some Id", Required = true)] int id
+       )
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            City = Summaries[Random.Shared.Next(Cities.Length)],
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    /// <summary>
+    /// Creates a new weather forecast
+    /// </summary>
+    /// <param name="name" example="New Your">The city name</param>
+    [HttpPost(Name = "CreateWeatherForecast")]
+    [SwaggerResponse(StatusCodes.Status201Created, "Returns the newly created item")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "If the item is null")]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, "Oops!")]
+    public WeatherForecast Create(
+         [FromBody, SwaggerRequestBody("The payload", Required = true)] string name
+        )
+    {
+        return new WeatherForecast
+        {
+            City = name,
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(4)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        };
+    }
+}
+
---
+++ OpenAPI.MVC/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using OpenAPI.MVC.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at
+// https://aka.ms/aspnetcore/swashbuckle
+// https://github.com/domaindrivendev/Swashbuckle.AspNetCore
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "OPEN API",
+        Description = "An OpenAPI ASP.NET Core Web API example",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    //Enrich OpenAPI metadata - requires the package Swashbuckle.AspNetCore.Annotations
+    options.EnableAnnotations();
+
+    // Alternative to the above way of annotating
+    // Adds XMl comments to swagger UI
+    // To generate the XML add <GenerateDocumentationFile>true</GenerateDocumentationFile> in the <PropertyGroup> to the csproj file
+    //var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    //options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // https://localhost:<port>/swagger/v1/swagger.json
+    // https://localhost:<port>/swagger/v1/swagger.yaml
+
+    app.UseSwagger(options =>
+    {
+        options.PreSerializeFilters.Add((swagger, httpReq) =>
+        {
+            swagger.Servers = new List<OpenApiServer>
+            {
+                // You can add as many OpenApiServer instances as you want by creating them like below
+                new OpenApiServer
+                {
+                    // You can set the Url from the default http request data or by hard coding it
+                    // Url = $"{httpReq.Scheme}://{httpReq.Host.Value}",
+                    Url = "https://api.my-weather-app.org/",
+                    Description = "Open Weather Map"
+                }
+            };
+        });
+    });   
+
+    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+    {
+        // Enables customization of the swagger ui - Remember to add app.UseStaticFiles()
+        options.InjectStylesheet("/swagger-ui/custom.css");
+        options.InjectJavascript("/swagger-ui/custom.js");
+
+        //options.SwaggerEndpoint("/api-docs/v1/swagger.json", "api-docs API");
+        //options.RoutePrefix = "api-docs"; //Changes path to the UI - default is "/swagger"
+
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // Ensure the wwwroot folder exists
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
